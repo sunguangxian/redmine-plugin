bd02e302d5c195fc0d3be4f5aa51fa5f6ba684ce
sunguangxian <38075908+sunguangxian@users.noreply.github.com>
2026-07-02 22:13:33 +0800

    Add project settings migration

---
+++ db/migrate/001_create_release_wiki_sync_project_settings.rb
@@ -0,0 +1,18 @@
+class CreateReleaseWikiSyncProjectSettings < ActiveRecord::Migration[6.1]
+  def change
+    create_table :release_wiki_sync_project_settings do |t|
+      t.integer :project_id, null: false
+      t.boolean :enabled, null: false, default: false
+      t.string :mode, null: false, default: 'single_list'
+      t.string :main_page
+      t.string :release_page_prefix
+      t.text :config_json
+      t.timestamps null: false
+    end
+
+    add_index :release_wiki_sync_project_settings,
+              :project_id,
+              unique: true,
+              name: 'idx_release_wiki_sync_project_settings_project'
+  end
+end
